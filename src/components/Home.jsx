4f9b6721bc5634ea3eda0a483ffc70732c1f0a9c
spbeal <62578556+spbeal@users.noreply.github.com>
2023-09-22 23:36:13 -0700

    Update main

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,15 +7,18 @@ const Home = () => {
 
       {/* Container */}
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-        <p>Hi, I am </p>
-        <h1>Samuel Beal</h1>
-        <h2>I'm a comp sci student at University of Idaho</h2>
-        <p> I'm a computer science student specializing in c++. I am currently building 
-          projects in c++ to expand my expertise and get real world experience. </p>
+        <p className='text-pink-600'>Hi, I am </p>
+        <h1 className='text-4xl sm:text-7x1 font-bold text-[#ccd6f6]'>Samuel Beal</h1>
+        <h2 className='text-3xl sm:text-7x1 py-2 font-bold text-[#8892b0]'>I'm a CS student at University of Idaho</h2>
+        <p className='text-[#8892b0] py-2 max-w-[700px]'> I'm a computer science student actively learning in C++. I am currently collaborating on
+          various projects to expand my expertise. </p>
           
       <div>
-        <p>--------------------------------------------------------------------------- </p>
-        <button> View Work <HiArrowNarrowRight /></button>
+        <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-700'> View Work 
+        <span className='group-hover:rotate-90 duration-300'>
+        <HiArrowNarrowRight className='ml-2'/>        
+        </span>
+        </button>
       </div>
 
       </div>

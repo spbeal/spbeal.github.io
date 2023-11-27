39dea17c841461cc11f7d3a6866f0b7f8c58b08f
spbeal <62578556+spbeal@users.noreply.github.com>
2023-11-27 00:21:20 -0800

    Add link to portfolio

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -12,13 +12,11 @@ const Home = () => {
     <div name='home' className='w-full h-screen bg-almond'> 
 
       {/* Container */}
-      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center items-center h-full '>
+      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center items-center h-full text-xl leading-8'>
         <h1 className='text-4xl font-bold sm:block border-b-4 border-pink-600'>Samuel Beal</h1>
 
-        <div className=''>
-        <p className=" mt-2 mb-3 text-2xl pt-1 font-extrabold tracking-tight text-text-color sm:text-4xl flex">
+        {/* <p className=" mt-2 mb-3 text-2xl pt-1 font-extrabold tracking-tight text-text-color sm:text-4xl flex">
         <p className=" hidden md:block whitespace-nowrap text-3xl font-extrabold">&lt;<span className="font-extrabold text-xl text-pink-600">div</span>&gt;
-        {/* <span className="text-2xl mx-2 font-extrabold leading-7 text-stone-400">I build</span></p> */}
         <span className="text-2xl mx-2 font-extrabold leading-7 text-[]">I build</span></p>
         <p className=" hidden md:block text-xs pt-3 font-black tracking-wide ">
         <TypeAnimation
@@ -41,9 +39,8 @@ const Home = () => {
         </p>
 
         <p className=" hidden md:block text-2xl font-bold pt-1  ">&lt;/<span className="text-xl pt-1 font-extrabold text-pink-600">div</span>&gt;</p>
-      </p>
-    </div>
-        <p className='hidden md:block text-center text-lg leading-8  py-2 max-w-[700px] '> 
+      </p> */}
+        <p className='hidden md:block text-center text-xl leading-8  py-2 max-w-[700px] '> 
         I am a Computer Science Student at University of Idaho. 
         Experienced in C++, basic python, and basic web development. When I'm not studying, 
         I am out with friends, playing water polo, or programming a new project 
@@ -55,6 +52,10 @@ const Home = () => {
         I am out with friends, playing water polo, or programming a new project 
          </p>
 
+         <p classname=''>
+            My portfolio is at
+           <a href='https://sambeal.dev/portfolio/'> sambeal.dev/portfolio</a>
+        </p>
           {/* Social icons */}
           <div className='py-4 grid grid-cols-2 text-4xl font-bold inline'>
             <div className='mx-3 '>
